314fb5207e805f7796e3ee62bae6a3e9f5bc6607
sparrowsl <benthorpe.dev@gmail.com>
2023-06-17 07:44:48 +0000

    display notes using django api

--- frontend/src/lib/NoteCard.svelte
+++ frontend/src/lib/NoteCard.svelte
@@ -1,16 +1,16 @@
 <script>
+  import dayjs from "dayjs";
   import Button from "./Button.svelte";
 
-  //  export let note = {};
+  export let note = {};
 </script>
 
 <div class="shadow-md border p-5 rounded">
-  <h2 class="text-gray-700 font-bold">Note title</h2>
-  <p class="text-(xs gray-400) italic mb-2">Created on {"note date"}</p>
-  <p class="text-(sm gray-500)">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus,
-    laudantium!
+  <h2 class="text-gray-700 font-bold">{note.title}</h2>
+  <p class="text-(xs gray-400) italic mb-2">
+    Created on {dayjs(note.date).format("DD MMMM, YYYY - HH:mm A")}
   </p>
+  <p class="text-(sm gray-500)">{note.descr}</p>
 
   <div class="flex justify-between items-center mt-5">
     <Button content="Edit" class="text-blue border-blue/40" />
